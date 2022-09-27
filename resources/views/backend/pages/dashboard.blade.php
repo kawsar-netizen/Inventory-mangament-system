5e71620391174ab5718fffa95406cd2c4d41c8ef
acquaintsam <acquaintsam@gmail.com>
2022-09-27 16:31:47 +0600

    new update

--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('backend.layouts.backend_master');
 
 @section('content_ims')
-  <h1 style="text-align: center;margin-top:40px;">Deshboard</h1>
+  <h1 style="text-align: center;margin-top:40px;">Dashboard</h1>
 @endsection
